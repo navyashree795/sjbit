a3fb6845841440507e86c22420fc95223658610c
navyashree795 <navyashree795@gmail.com>
2026-04-29 17:15:48 +0530

    gg

--- mobile-companion/firebase.js
+++ mobile-companion/firebase.js
@@ -1,13 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC295ZCOb7tXpT0RpT0Ezi8_gXbgiZU0ns",
   authDomain: "adaptly-524b9.firebaseapp.com",
+  databaseURL: "https://adaptly-524b9-default-rtdb.firebaseio.com",
   projectId: "adaptly-524b9",
   storageBucket: "adaptly-524b9.firebasestorage.app",
   messagingSenderId: "679466843497",
@@ -17,9 +19,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
-});
-
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
